0b278df5102d7506f05543fb246cbf009938926c
Shunfa Xu <gouwa5700@gmail.com>
2025-05-18 16:36:36 +0000

    add Tk toolkit as dependency for building python with brew

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,7 +6,7 @@
 sudo apt update
 
 # one-liner
-sudo apt install -y git openssh-server curl vim zsh build-essential tmux
+sudo apt install -y git openssh-server curl vim zsh build-essential tmux tk-dev
 
 ## On-my-zsh
 
